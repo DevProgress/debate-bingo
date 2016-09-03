b32a36ece25546fd906b4861df486209b9a71d73
Katie Schick <kkschick@gmail.com>
2016-09-03 11:28:41 -0400

    Make sure free sq never gets a button (#9)

--- src/BingoRow.js
+++ src/BingoRow.js
@@ -10,13 +10,14 @@ export default class BingoRow extends React.Component {
                 return this.props.onTileDaubed(i);
             }.bind(this);
             let key = `tile-${i}`,
-                tileText = rowDaubs[i] === -1 ? 'Free Square!': tile.text;
+                isFreeSquare = this.props.rowIndex === 2 && i === 2,
+                tileText = isFreeSquare ? 'Free Square!': tile.text;
             return (
                 <BingoTile
                     colorClass={tile.colorClass}
-                    isDaubed={rowDaubs[i] === 1 || rowDaubs[i] === -1}
+                    isDaubed={rowDaubs[i] === 1 || isFreeSquare}
                     isHighlighted={rowDaubs[i] === 2}
-                    isFreeSquare={rowDaubs[i] === -1}
+                    isFreeSquare={isFreeSquare}
                     onTileDaubed={onTileDaubed}
                     key={key}
                     tileText={tileText}>
