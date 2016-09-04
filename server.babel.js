7638f0687f091d70474038dcc0e623440386d9af
Jonathan Keslin <github@jonathankeslin.com>
2016-09-04 16:36:57 -0700

    Add methods to server to pull random set of terms
    Add methods to server to pull a random set of bingo terms based on the card type requested. Methods are not used yet though.

--- server.babel.js
+++ server.babel.js
@@ -2,6 +2,30 @@ import express from 'express';
 
 const app = express();
 
+function getRandomSet(arr, len) {
+    var ret = [];
+    var used = [];
+    for(var i=0; i<len; i++) {
+        var idx;
+        do {
+            idx = Math.floor(Math.random() * arr.length);
+        } while(used.indexOf(idx) !== -1);
+        used.push(idx);
+        ret.push(arr[idx]);
+    }
+    return ret;
+}
+function getTerms(termsFile) {
+    var lineReader = require('readline').createInterface({
+        input: require('fs').createReadStream(termsFile)
+    });
+    var ret = [];
+    lineReader.on('line', function (line) {
+        ret.push(line);
+    });
+    return ret;
+} 
+
 app.use('/', express.static('dist'));
 app.get('/api/getCardData/:type', function(req, res) {
     var options = {
