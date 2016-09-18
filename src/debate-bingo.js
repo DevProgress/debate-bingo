8ef80eafb1669048944eb61f07aa3f2c5c685e5d
andy timmons <andrewjtimmons@gmail.com>
2016-09-18 12:19:34 -0700

    add images on win

--- src/debate-bingo.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Styles from './styles.scss';
-import BingoApp from './BingoApp';
-
-ReactDOM.render(
-    <BingoApp />,
-    document.getElementById("content")
-);
